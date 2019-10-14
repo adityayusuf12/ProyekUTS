3277aa0af9aa6a6616fda537ac8665eedb078781
adityayusuf12 <ayraharja88@gmail.com>
2019-10-14 22:11:42 +0700

    layout home

---
+++ app/src/main/java/com/example/proyekuts/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.proyekuts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Dolar(View view) {
+    }
+
+    public void Pound(View view) {
+    }
+
+    public void Euro(View view) {
+    }
+
+    public void yen(View view) {
+    }
+}
