6ed38aed5857357d45b006fbf8e52795fe17f816
adityayusuf12 <ayraharja88@gmail.com>
2019-10-22 21:46:17 +0700

    Menambahkan toast

--- app/src/main/java/com/example/proyekuts/DolarActivity.java
+++ app/src/main/java/com/example/proyekuts/DolarActivity.java
@@ -3,40 +3,54 @@ package com.example.proyekuts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.NumberFormat;
-import java.util.Locale;
-
 public class DolarActivity extends AppCompatActivity {
    EditText edit_angka;
    Button convert;
    TextView hasil;
 
-   double angka;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dolar);
 
-        edit_angka =  findViewById(R.id.rupiah_inpt);
+        edit_angka =  findViewById(R.id.dolar_inpt);
         convert =  findViewById(R.id.button_convert);
         hasil =  findViewById(R.id.TV_output);
 
-
-
+//        convert.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (TextUtils.isEmpty(edit_angka.getText())){
+//                    Toast.makeText(DolarActivity.this, "masukkan angka", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Convert();
+//                }
+//            }
+//        });
 
     }
 
-    public void Convert(View view) {
+    public void Convert() {
 
         String getAngka = edit_angka.getText().toString();
-        double angs = Double.parseDouble(getAngka);
-        double hasildlr = angs * 14150;
-        hasil.setText("$" + hasildlr);
+        double angka = Double.parseDouble(getAngka);
+        double hasildlr = angka * 14150;
+        hasil.setText("Rp" + hasildlr);
+    }
+
+    public void convert(View view) {
+        if (TextUtils.isEmpty(edit_angka.getText())){
+                    Toast.makeText(DolarActivity.this, "masukkan angka", Toast.LENGTH_SHORT).show();
+                }else{
+                    Convert();
+                }
     }
 }
--- app/src/main/java/com/example/proyekuts/YenActivity.java
+++ app/src/main/java/com/example/proyekuts/YenActivity.java
@@ -3,12 +3,41 @@ package com.example.proyekuts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class YenActivity extends AppCompatActivity {
+    EditText inpt;
+    Button convert;
+    TextView hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_yen);
+        inpt = findViewById(R.id.yen_inpt);
+        convert = findViewById(R.id.button_convert);
+        hasil = findViewById(R.id.TV_output);
+
+    }
+
+    public void convert(View view) {
+        if (TextUtils.isEmpty(inpt.getText())){
+            Toast.makeText(YenActivity. this, "Masukan Angka", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            String getInpt = inpt.getText().toString();
+            double angka = Double.parseDouble(getInpt);
+            double hsl = angka * 130;
+            hasil.setText("Rp"+ hsl);
+    }
+
+
     }
 }
